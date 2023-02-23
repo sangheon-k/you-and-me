9abeaee5d7cf7357bf990eb76f2e486825c98a85
sangheon <sangheon@sangheonui-MacBookAir.local>
2023-02-23 23:28:38 +0900

    add : socket connection hook

---
+++ client/src/components/Room/Room.jsx
@@ -0,0 +1,61 @@
+import { useSocket } from '@/src/hooks/useSocket';
+import React, { useEffect, useState } from 'react';
+
+const Room = ({ enteredRoomName }) => {
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    socket.on('welcome', (user) => {
+      setMessageList((prev) => [...prev, `${user} join!`]);
+    });
+
+    socket.on('bye', (user) => {
+      setMessageList((prev) => [...prev, `${user} left this room ㅠㅠ`]);
+    });
+
+    socket.on('newMessage', (message) => {
+      setMessageList((prev) => [...prev, message]); // 남이 보낸 메시지 리스트에 추가
+    });
+
+    return () => {
+      socket.off('welcome');
+      socket.off('bye');
+      socket.off('newMessage');
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('newMessage', message, enteredRoomName, () => {
+      setMessageList((prev) => [...prev, `You : ${message}`]); // 내가 보낸 메시지 리스트에 추가
+      setMessage('');
+    });
+  };
+
+  return (
+    <div>
+      <h1>Room {enteredRoomName}</h1>
+      <ul>
+        {messageList?.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+      <form action='#none' onSubmit={(e) => e.preventDefault()}>
+        <input
+          type='text'
+          name='message'
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)}
+          value={message}
+        />
+        <button type='submit' onClick={handleSubmit}>
+          send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Room;
