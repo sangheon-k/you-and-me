600edf95dcaf4fe53ff2bd1b695ad1bf23efec1a
sangheon-k <sangheon.k@kakao.com>
2023-03-15 13:35:31 +0900

    add : Seo Component

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,15 +1,11 @@
-import Head from 'next/head';
-import '@/src/styles/globals.css'
-
+import Seo from '../components/Seo';
+import '@/src/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" /> 
-      </Head>
+      <Seo />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
