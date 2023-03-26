af495b60c9b8145c9f206dd21c76fdb8f717e719
sangheon-k <sangheon.k@kakao.com>
2023-03-26 13:11:56 +0900

    modify : chat room ui

--- client/src/components/Chat/Room/Room.jsx
+++ client/src/components/Chat/Room/Room.jsx
@@ -37,15 +37,20 @@ const Room = ({ enteredRoomName }) => {
   return (
     <div>
       <h1>Room {enteredRoomName}</h1>
-      <ul>
+      <div className=' overflow-y-scroll h-[600px] py-3'>
         {messageList?.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return <div key={index}>{item}</div>;
         })}
-      </ul>
-      <form action='#none' onSubmit={(e) => e.preventDefault()}>
+      </div>
+      <form
+        action='#none'
+        className='flex gap-4'
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
           type='text'
           name='message'
+          className='w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-4 text-base font-medium text-[#6B7280] outline-none focus:border-blue-500 focus:shadow-md'
           onChange={(e) => setMessage(e.target.value)}
           onKeyDown={(e) =>
             e.key === 'Enter' &&
@@ -54,7 +59,11 @@ const Room = ({ enteredRoomName }) => {
           }
           value={message}
         />
-        <button type='submit' onClick={handleSubmit}>
+        <button
+          type='submit'
+          className='block px-6 py-3 text-white bg-blue-500 rounded-lg shadow-lg md:inline-block'
+          onClick={handleSubmit}
+        >
           send
         </button>
       </form>
